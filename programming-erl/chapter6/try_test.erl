5e1a43e8446e6120a516cdca4d032e2ca67b56a8
Curtis Wan <wcy9988@163.com>
2022-12-10 17:26:25 +0800

    learning

--- programming-erl/chapter6/try_test.erl
+++ programming-erl/chapter6/try_test.erl
@@ -1,5 +1,5 @@
 -module(try_test).
--export([demo1/0]).
+-export([demo1/0, demo2/0, demo3/0]).
 
 generate_exception(1) -> a;
 generate_exception(2) -> throw(a);
@@ -16,4 +16,15 @@ catcher(N) ->
         throw:X -> {N, caught, thrown, X};
         exit:X -> {N, caught, exited, X};
         error:X -> {N, caught, error, X}
+    end.
+
+demo2() ->
+    [{I, (catch generate_exception(I))} || I <- lists:seq(1, 5)].
+
+demo3() ->
+    try
+        generate_exception(2)
+    catch
+        Class:ExceptionPattern:Stacktrace ->
+            {Class, ExceptionPattern, Stacktrace}
     end.
