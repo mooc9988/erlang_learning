ecfa6719de1353cd7627341f95c1aa31ab8a9e03
Curtis Wan <wcy9988@163.com>
2022-12-22 14:54:02 +0800

    finish chapter25

--- programming-erl/chapter25/joe_test/src/joe_test.erl
+++ programming-erl/chapter25/joe_test/src/joe_test.erl
@@ -2,4 +2,17 @@
 -export([start/0]).
 
 start() ->
-    io:format("Hello my love").
+    Handle = bitcask:open("bertie_database", [read_write]),
+    N = fetch(Handle),
+    store(Handle, N+1),
+    io:format("Bertie has been run ~p times ~n", [N]),
+    bitcask:close(Handle),
+    init:stop().
+
+store(Handle, N) ->
+    bitcask:put(Handle, <<"bertie_excutions">>, term_to_binary(N)).
+fetch(Handle) ->
+    case bitcask:get(Handle, <<"bertie_excutions">>) of
+        not_found -> 1;
+        {ok, Bin} -> binary_to_term(Bin)
+    end.
